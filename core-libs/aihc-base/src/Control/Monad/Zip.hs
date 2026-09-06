4622ff657af2d415cb2402af59e04059cc70ac5c
David Himmelstrup <lemmih@gmail.com>
2026-09-06 14:27:50 +0200

    feat(aihc-base): fill in the Data.Functor.Classes, Contravariant, MonadZip, Foldable1, Bifoldable, and Bitraversable stubs
    Data.Functor.Classes, Data.Functor.Contravariant, Control.Monad.Zip,
    Data.Foldable1, Data.Bifoldable, and Data.Bitraversable were empty
    modules, so every package importing them (transformers among others)
    failed name resolution. Each now provides the base API with the
    Prelude instances that aihc-base can express.
    
    Supporting instances: Foldable NonEmpty in GHC.Internal.Foldable,
    and Bifunctor for triples, quadruples, and Const.
    
    Five eval fixtures exercise the modules from the source tree.

--- core-libs/aihc-base/src/Control/Monad/Zip.hs
+++ core-libs/aihc-base/src/Control/Monad/Zip.hs
@@ -1,1 +1,48 @@
-module Control.Monad.Zip () where
+{-# LANGUAGE KindSignatures #-}
+
+module Control.Monad.Zip
+  ( MonadZip (..),
+  )
+where
+
+import Data.Functor.Identity (Identity (..))
+import Data.Kind (Type)
+import Data.List.NonEmpty (NonEmpty (..))
+import Data.List.NonEmpty qualified as NonEmpty
+import Prelude
+
+{- HLINT ignore "Use (,)" -}
+{- HLINT ignore "Avoid NonEmpty.unzip" -}
+
+-- | Instances should satisfy the laws:
+--
+-- [Naturality] @'fmap' (f '***' g) ('mzip' ma mb) = 'mzip' ('fmap' f ma) ('fmap' g mb)@
+-- [Information preservation] if @'fmap' ('const' ()) ma = 'fmap' ('const' ()) mb@ then @'munzip' ('mzip' ma mb) = (ma, mb)@
+class (Monad m) => MonadZip (m :: Type -> Type) where
+  mzip :: m a -> m b -> m (a, b)
+  mzip = mzipWith (\a b -> (a, b))
+
+  mzipWith :: (a -> b -> c) -> m a -> m b -> m c
+  mzipWith f ma mb = fmap (uncurry f) (mzip ma mb)
+
+  munzip :: m (a, b) -> (m a, m b)
+  munzip mab = (fmap fst mab, fmap snd mab)
+
+instance MonadZip [] where
+  mzip = zip
+  mzipWith = zipWith
+  munzip = unzip
+
+instance MonadZip Maybe where
+  mzipWith f (Just a) (Just b) = Just (f a b)
+  mzipWith _ Nothing _ = Nothing
+  mzipWith _ (Just _) Nothing = Nothing
+
+instance MonadZip Identity where
+  mzipWith f (Identity a) (Identity b) = Identity (f a b)
+  munzip (Identity (a, b)) = (Identity a, Identity b)
+
+instance MonadZip NonEmpty where
+  mzip = NonEmpty.zip
+  mzipWith = NonEmpty.zipWith
+  munzip = NonEmpty.unzip
--- core-libs/aihc-base/src/Data/Bifunctor.hs
+++ core-libs/aihc-base/src/Data/Bifunctor.hs
@@ -5,6 +5,7 @@ module Data.Bifunctor
   )
 where
 
+import Control.Applicative (Const (..))
 import Data.Kind (Type)
 import Prelude (Either (..), id, (.))
 
@@ -26,3 +27,12 @@ instance Bifunctor (,) where
 instance Bifunctor Either where
   bimap onFirst _ (Left value) = Left (onFirst value)
   bimap _ onSecond (Right value) = Right (onSecond value)
+
+instance Bifunctor ((,,) x) where
+  bimap onFirst onSecond (x, valueOne, valueTwo) = (x, onFirst valueOne, onSecond valueTwo)
+
+instance Bifunctor ((,,,) x y) where
+  bimap onFirst onSecond (x, y, valueOne, valueTwo) = (x, y, onFirst valueOne, onSecond valueTwo)
+
+instance Bifunctor Const where
+  bimap onFirst _ (Const value) = Const (onFirst value)
