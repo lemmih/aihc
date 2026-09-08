b17df714823529b197fda78758f164d581bbb30b
David Himmelstrup <lemmih@gmail.com>
2026-09-08 14:31:56 +0000

    feat(tc): never bind a meta-variable to a polytype (#1903)

--- components/aihc-tc/src/Aihc/Tc/Generate/Expr.hs
+++ components/aihc-tc/src/Aihc/Tc/Generate/Expr.hs
@@ -572,7 +572,7 @@ inferApplication sp rebuild fun arg = do
   zonkedFunTy <- zonkType funTy
   case zonkedFunTy of
     TcFunTy expectedArgTy resultTy
-      | hasLeadingForAll expectedArgTy -> do
+      | isPolyType expectedArgTy -> do
           (arg', argCts) <- checkHigherRankArgument sp expectedArgTy arg
           pure (rebuild fun' arg', resultTy, funCts <> argCts)
       | otherwise -> do
@@ -597,7 +597,7 @@ inferApplication sp rebuild fun arg = do
 instantiateFunctionType :: SourceSpan -> Expr -> TcType -> TcM (Expr, TcType, [Ct])
 instantiateFunctionType sp fun funTy = do
   zonked <- zonkType funTy
-  if hasLeadingForAll zonked
+  if isPolyType zonked
     then do
       (instantiated, typeArgs, predicates) <- instantiateSigmaType zonked
       cts <- mapM (predToCt sp "<application>") predicates
@@ -655,11 +655,6 @@ checkHigherRankArgument sp expectedTy arg = do
       pure $ case result of
         DictSolved -> []
         DictStuck stuck -> [stuck]
-
-hasLeadingForAll :: TcType -> Bool
-hasLeadingForAll TcForAllTy {} = True
-hasLeadingForAll TcQualTy {} = True
-hasLeadingForAll _ = False
 
 instantiateSigmaType :: TcType -> TcM (TcType, [TcType], [Pred])
 instantiateSigmaType = go []
